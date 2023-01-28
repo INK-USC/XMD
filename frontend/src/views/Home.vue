5c6e0568f3a6f4619ddd30e616545421c11b8733
Dong-Ho Lee <donghole@usc.edu>
2023-01-27 20:06:35 -0800

    tmp demo

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,7 +1,9 @@
 <template>
   <h1 style="text-align: center">Home page of HILT Demo</h1>
+  <div style="display: flex; justify-content: center">
+    <p> (Guideline will be uploaded soon) </p>
+  </div>
 
-  <p> Click Projects (Guideline will be uploaded soon) </p>
 </template>
 
 <script>
