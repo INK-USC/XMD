041bfd2fe3f1bc179db9800ea00df2a30796f751
Dong-Ho Lee <donghole@usc.edu>
2023-02-23 11:36:04 -0800

    feat: tutorial popup

---
+++ frontend/src/components/project/TutorialModal.vue
@@ -0,0 +1,38 @@
+<template>
+  <el-dialog
+    :title="'Tutorial'"
+    v-model="dialogVisible"
+    v-on:update:visible="$emit('update:dialogVisible', $event)"
+    @close="$emit('update:dialogVisible', false)"
+    width="40%"
+  >
+    <div>
+
+    </div>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="() => $emit('update:dialogVisible', false)">
+          Understand
+        </el-button>
+      </span>
+    </template>
+  </el-dialog>
+</template>
+
+<script>
+
+// the form when user create/edit a project
+export default {
+  name: "TutorialModal",
+  props: { dialogVisible: Boolean, existingInfo: Object },
+  setup() {
+  },
+  data() {
+    return {};
+  },
+  methods: {
+  },
+};
+</script>
+
+<style scoped></style>
